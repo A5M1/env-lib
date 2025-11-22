794f496b0497b62093e8db318496e23636326cc3
gtkn <admin@abby0666.xyz>
2025-11-21 17:06:50 -0800

    Update build script and add .env file generation
    Adds a call to devcmd.bat for environment setup, renames LIB variable to ENV_LIB, and generates a .env file in the output directory. Also adds a pause at the end of the script for easier review of build output.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,11 @@
 @echo off
+call e:\buildtools\devcmd.bat
+
 set ENV_DIR=env
 set TEST_DIR=test
 set OBJ_DIR=obj
 set BIN_DIR=bin
-set LIB=%BIN_DIR%\env.lib
+set ENV_LIB=%BIN_DIR%\env.lib
 set TARGET=%BIN_DIR%\test.exe
 
 if not exist %OBJ_DIR% mkdir %OBJ_DIR%
@@ -11,13 +13,20 @@ if not exist %BIN_DIR% mkdir %BIN_DIR%
 
 for %%f in (%ENV_DIR%\*.c) do cl /c /nologo /W3 /O2 /I %ENV_DIR% %%f /Fo%OBJ_DIR%\%%~nf.obj
 
-lib /nologo /OUT:%LIB% %OBJ_DIR%\*.obj
+lib /nologo /OUT:%ENV_LIB% %OBJ_DIR%\*.obj
 
 for %%f in (%TEST_DIR%\*.c) do cl /c /nologo /W3 /O2 /I %ENV_DIR% /I %TEST_DIR% %%f /Fo%OBJ_DIR%\%%~nf.obj
 
-cl /nologo /Fe%TARGET% %OBJ_DIR%\*.obj %LIB%
+cl /nologo /Fe%TARGET% %OBJ_DIR%\*.obj %ENV_LIB%
+
+:: --- create .env file in output directory ---
+(
+echo API_KEY=foo
+echo USER=foo
+) > %BIN_DIR%\.env
 
 echo Generating documentation...
 doxygen Doxyfile
 
 echo Build complete.
+pause
